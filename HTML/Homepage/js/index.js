3245dcce0e712fd2368e46ae82b0f1b871f5a2c4
Luca <63116530+EckigerLuca@users.noreply.github.com>
2022-09-14 11:14:22 +0200

    14.09.2022 (morgens)

---
+++ HTML/Homepage/js/index.js
@@ -0,0 +1,21 @@
+// Get the button:
+window.addEventListener('load', (event) => {
+	let topBtn = document.getElementById("topBtn");
+})
+
+// When the user scrolls down 60px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
