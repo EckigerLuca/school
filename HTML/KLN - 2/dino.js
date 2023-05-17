6b616a43702d9ac456651ede7500595f66619115
EckigerLuca <63116530+EckigerLuca@users.noreply.github.com>
2023-05-17 14:23:30 +0200

    dino but better

--- HTML/KLN - 2/dino.js
+++ HTML/KLN - 2/dino.js
@@ -65,7 +65,7 @@ function handleJump(delta) {
 }
 
 function onJump(e) {
-	if (e.code !== "Space" || isJumping) return;
+	if ((e.code !== "Space" && e.code !== "ArrowUp") || isJumping) return;
 
 	yVelocity = JUMP_SPEED;
 	isJumping = true;
